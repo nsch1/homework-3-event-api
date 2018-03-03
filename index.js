4c7b69daef155a3d9f11e55b83f83e693d57631f
nsch1 <nick.schooneman1@gmail.com>
2018-03-03 16:17:02 +0100

    Add cors to index

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
 const port = process.env.PORT || 4001
+
+app.use(cors())
 
 app.listen(port, () => {
   console.log(`Express listening on port: ${port}`)
