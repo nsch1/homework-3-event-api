f7ee5e23c373016eeb923b634efe6983a21fc73d
nsch1 <nick.schooneman1@gmail.com>
2018-03-03 16:20:51 +0100

    Add bodyparser and basic events router

---
+++ events/router.js
@@ -0,0 +1,6 @@
+const Router = require('express').Router
+const {Event} = require('../models')
+
+const router = new Router()
+
+module.exports = router
--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const bodyParser = require('body-parser')
 const port = process.env.PORT || 4001
+const eventsRouter = require('./events/router')
 
 app.use(cors())
+app.use(bodyParser.json())
+app.use(eventsRouter)
 
 app.listen(port, () => {
   console.log(`Express listening on port: ${port}`)
